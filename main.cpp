8c5bd39d90ed29ba2ca8d6373a01c7a2e1441acb
noeandrewanderson <119889139+noeandrewanderson@users.noreply.github.com>
2022-12-07 10:45:31 -0800

    added libraries

--- main.cpp
+++ main.cpp
@@ -4,3 +4,4 @@
 #include <string>
 
 using namespace std;
+
--- sud.cpp
+++ sud.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <algorithm>
+#include <iterator>
+
 using namespace std;
 
 char welcomeUser(){
