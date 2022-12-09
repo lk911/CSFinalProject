ab90cadb06fc2e81659c58e0ca114646844ec015
lk911 <30807058+lk911@users.noreply.github.com>
2022-12-08 19:46:30 -0800

    Update sud.h

--- sud.h
+++ sud.h
@@ -1,8 +1,14 @@
 #ifndef SUD_H
 #define SUD_H
-using namespace std;
 char welcomeUser();
 void createSolution(int solution[9][9]);
 void createPuzzle();
-
+void generateBoard(int solution[9][9], int startX, int startY);
+void makeBoard(int solution[9][9]);
+bool validBoard(int solution[9][9]);
+void printBoard(int solution[9][9]);
+void makePuzzle(int presetSolution[9][9],char choice);
+void initSudoku();   
+bool enterandcheckanswer(int solution[9][9],int presetSolution[9][9]);
+bool isfull(int originalSolution[9][9]);
 #endif
