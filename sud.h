2d3c830814b3bebaba3015b21f5220ccb1948cae
Cyanonitrile <93655109+Cyanonitrile@users.noreply.github.com>
2022-12-08 22:54:17 -0800

    Update sud.h

--- sud.h
+++ sud.h
@@ -1,14 +1,15 @@
 #ifndef SUD_H
 #define SUD_H
-
+char welcomeUser();
 void createSolution(int solution[9][9]);
 void createPuzzle();
 void generateBoard(int solution[9][9], int startX, int startY);
 void makeBoard(int solution[9][9]);
 bool validBoard(int solution[9][9]);
 void printBoard(int solution[9][9]);
-void makePuzzle(int presetSolution[9][9]);
+void makePuzzle(int presetSolution[9][9],char choice);
 void initSudoku();   
-bool checkAnswer(int presetSolution[9][9]);
-
+bool enterandcheckanswer(int solution[9][9],int presetSolution[9][9], int &numguesses, int &numtrueguesses,int &numfalseguesses);
+bool isfull(int presetSolution[9][9]);
+void outputStats(int numguesses, int numtrueguesses, int numfalseguesses);
 #endif
