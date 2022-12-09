486b28a534554c137b0ad2de5db47cfb7779f73b
lk911 <lkords@aol.com>
2022-12-08 17:47:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,31 +2,35 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cstdio>
+#include <ctime>
 
 using namespace std;
 
 int main(){
-int presetSolution[9][9] = {
-    {5,3,4,6,7,8,9,1,2},
-    {6,7,2,1,9,5,3,4,8},
-    {1,9,8,3,4,2,5,6,7},
-    {8,5,9,7,6,1,4,2,3},
-    {4,2,6,8,5,3,7,9,1},
-    {7,1,3,9,2,4,8,5,6},
-    {9,6,1,5,3,7,2,8,4},
-    {2,8,7,4,1,9,6,3,5},
-    {3,4,5,2,8,6,1,7,9}
+    clock_t start;
+    double duration;
+    start = clock();
+    duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
+    int presetSolution[9][9] = {
+        {5,3,4,6,7,8,9,1,2},
+        {6,7,2,1,9,5,3,4,8},
+        {1,9,8,3,4,2,5,6,7},
+        {8,5,9,7,6,1,4,2,3},
+        {4,2,6,8,5,3,7,9,1},
+        {7,1,3,9,2,4,8,5,6},
+        {9,6,1,5,3,7,2,8,4},
+        {2,8,7,4,1,9,6,3,5},
+        {3,4,5,2,8,6,1,7,9}
 
-                          };
-int solution[9][9];
-srand(time(0));
-makeBoard(presetSolution);
+                            };
+    int solution[9][9];
+    srand(time(0));
+    makeBoard(presetSolution);
 
-for(int i=0; !checkAnswer(presetSolution); i++ ){
-    
-}
-
-
-return 0;
+    for(int i=0; !checkAnswer(presetSolution); i++ ){
+        
+    }
+    return 0;
 }
 
